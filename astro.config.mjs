e38054e652d7efab2e5ad523c4c159033bf50107
pcktm <jakub@kopanko.com>
2023-02-27 15:20:16 +0100

    feat: new image loader & contact changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,9 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 import compress from "astro-compress";
 
+import { astroImageTools } from "astro-imagetools";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), prefetch(), compress()]
+  integrations: [react(), tailwind(), prefetch(), astroImageTools, compress()]
 });
