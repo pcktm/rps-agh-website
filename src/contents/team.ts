f4ff48c15dba88e9951c01b728787732027f5060
pcktm <jakub@kopanko.com>
2023-11-25 15:28:56 +0100

    feat: new pictures

--- src/contents/team.ts
+++ src/contents/team.ts
@@ -1,6 +1,6 @@
 import msajdakimg from '../assets/msajdak.jpg'
 import azatykaimg from '../assets/azatyka.jpg'
-import wplacekimg from '../assets/wplacek.jpg'
+import marybarczykimg from '../assets/mrybarczyk.jpg'
 import dslomnickiimg from '../assets/dslomnicki.jpg'
 
 export type TeamMember = {
@@ -24,21 +24,21 @@ const team: TeamMember[] = [
     role: "Asystentka Rzecznika",
     email: "malgorzata.sajdak@samorzad.agh.edu.pl",
     src: msajdakimg,
-    description: `Studentka III roku Inżynierii i Zarządzania Procesami Przemysłowymi na Wydziale Inżynierii Lądowej i Gospodarki Zasobami. Od 2021 członek Komisji ds. Dydaktyki URSS AGH. Swój czas wolny spędza grając w koszykówkę lub biegając. Chętnie ogląda seriale, głównie te dramatyczne.`
+    description: `Studentka IV roku Inżynierii i Zarządzania Procesami Przemysłowymi na Wydziale Inżynierii Lądowej i Gospodarki Zasobami. Od 2021 członek Komisji ds. Dydaktyki URSS AGH. Swój czas wolny spędza grając w koszykówkę lub biegając. Chętnie ogląda seriale, głównie te dramatyczne.`
   },
   {
     name: "Dawid Słomnicki",
     role: "Asystent Rzecznika",
     email: "dawid.slomnicki@samorzad.agh.edu.pl",
     src: dslomnickiimg,
-    description: `Student III roku Informatyki i Systemów Inteligentnych na Wydziale Elektrotechniki, Automatyki, Informatyki i Inżynierii Biomedycznej. Od roku 2021 członek Komisji ds. Dydaktyki i Jakości Kształcenia URSS. Miłośnik dobrego kodu i regulaminu.`
+    description: `Student IV roku Informatyki i Systemów Inteligentnych na Wydziale Elektrotechniki, Automatyki, Informatyki i Inżynierii Biomedycznej. Od roku 2021 członek Komisji ds. Dydaktyki i Jakości Kształcenia URSS. Miłośnik dobrego kodu i regulaminu.`
   },
   {
-    name: "Wiktoria Placek",
+    name: "Magdalena Rybarczyk",
     role: "Promocja Rzecznika",
-    email: "wiktoria.placek@samorzad.agh.edu.pl",
-    src: wplacekimg,
-    description: `Studentka IV roku Technologii Chemicznej na Wydziale Energetyki i Paliw. Od 2021 członek Komisji ds. Promocji URSS AGH. W zespole RPS odpowiada za promocję. W wolnym czasie jeździ na rolkach i działa w straży pożarnej.`
+    email: "magdalena.rybarczyk@samorzad.agh.edu.pl",
+    src: marybarczykimg,
+    description: `Studentka IV roku Inżynierii i Zarządzania Procesami Przemysłowymi na Wydziale Inżynierii Lądowej i Gospodarki Zasobami. Od 2021 roku Pełnomocnik Zarządu WRSS WILiGZ AGH, a od 2022 członek Komisji ds. Dydaktyki URSS AGH. W zespole RPS odpowiada za promocję. Chętnie podejmuje się nowych wyzwań i odkrywa to, co nieznane.`
   }
 ]
 
